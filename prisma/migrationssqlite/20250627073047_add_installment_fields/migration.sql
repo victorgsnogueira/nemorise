3a3e8cd333756bdee5ccfa05d53b8d24bc555114
Victor Nogueira <vgdsno@gmail.com>
2025-06-27 04:52:30 -0300

    delete migrations sqlite

--- prisma/migrationssqlite/20250627073047_add_installment_fields/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE "Expense" ADD COLUMN "installmentGroupId" TEXT;
-ALTER TABLE "Expense" ADD COLUMN "installmentNumber" INTEGER;
-ALTER TABLE "Expense" ADD COLUMN "totalInstallments" INTEGER;
